0a7ffd014abee68d3228f421d57b42b86f63859b
billythekid <billy@the-kid.org>
2016-10-27 21:50:44 +0100

    tweaks to weather sunrise and sunset. sun animation. re-working of hours/minutes to 24hr clock. (i renamed the main css file to scss because that's how my system's set up - also, the minified javascript isn't anymore.)

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,33 +1,49 @@
-loadWeather("Fort William, Scotland");
-$(document).ready(function(){
-    setInterval(loadWeather,10000);
-});
-function loadWeather(location, woeid){
-    $.simpleWeather({
-        location: location,
-        woeid: woeid,
-        unit: 'C',
-        success: function(weather){
-            temp = '<p>'+weather.temp + '&deg;'+'C'+'</p>';
-            wcode = '<object class="icon" data="assets/img/svg/weathericons/'+weather.code+'.svg">';
-            cur = '<p>'+weather.currently+'</p>';
-            winds = '<p>'+weather.wind.speed+weather.units.speed+'</p>';
-            windd = '<p>'+weather.wind.direction+'</p>';
-            sunu= '<p>'+weather.sunrise+'</p>';
-            sund= '<p>'+weather.sunset+'</p>';
-            visi= '<p>'+weather.visibility+weather.units.distance+'</p>';
-            $(".temperature").html(temp);
-            $(".climate_bg").html(wcode);
-            $(".windspeed").html(winds);
-            $(".direction").html(windd);
-            $(".currently").html(cur);
-            $(".sun-up").html(sunu);
-            $(".sun-down").html(sund);
-            $(".visibility").html(visi);
-
-        },
-        error: function(error){
-            $(".error").html('<p>'+error+'</p>');
+function getHoursAndMinutesFromTimeString(hopelessTime) {
+    var timeAndOrdinal = hopelessTime.split(' ');
+    var amOrPm = timeAndOrdinal[1];
+    var time = timeAndOrdinal[0].split(':').map(Number);
+    if (time[0] == 12) {
+        time[0] = (amOrPm == 'am') ? 0 : 12;
+    } else {
+        if (amOrPm == 'pm') {
+            time[0] += 12;
         }
-    });
+    }
+    time = time.map(String);
+    for (var i = 0; i < time.length; i++) {
+        time[i] = (time[i].length == 1) ? '0' + time[i] : '' + time[i];
+    }
+    return time;
 }
+function loadWeather(e, t) {
+    $.simpleWeather({
+        location: e,
+        woeid: t,
+        unit: "C",
+        success: function (e) {
+            sunriseTime = getHoursAndMinutesFromTimeString('12:03 am');
+            sunsetTime = getHoursAndMinutesFromTimeString(e.sunset);
+            temp = "<p>" + e.temp + "&deg;C</p>",
+                wcode = '<object class="icon" data="assets/img/svg/weathericons/' + e.code + '.svg">',
+                cur = "<p>" + e.currently + "</p>",
+                winds = "<p>" + e.wind.speed + e.units.speed + "</p>",
+                windd = "<p>" + e.wind.direction + "</p>",
+                sunu = "<p>" + sunriseTime.join(':') + "</p>",
+                sund = "<p>" + sunsetTime.join(':') + "</p>",
+                $(".temperature").html(temp),
+                $(".climate_bg").html(wcode),
+                $(".windspeed").html(winds),
+                $(".direction").html(windd),
+                $(".currently").html(cur),
+                $(".sun-up").html(sunu),
+                $(".sun-down").html(sund)
+        },
+        error: function (e) {
+            $(".error").html("<p>" + e + "</p>")
+        }
+    })
+}
+loadWeather("Fort William, Scotland"),
+    $(document).ready(function () {
+        setInterval(loadWeather, 1e4)
+    });
